265ebadbb7475bfff665fed50988ed2872aac033
kim haeun <35323579+saeloi@users.noreply.github.com>
2019-05-29 11:28:45 +0900

    Add files via upload

---
+++ A008_MouseUp/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace A008_MouseUp
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void grid1_MouseUp(object sender, MouseButtonEventArgs e)  //객체 e 메소드 안에 getposition지정
+        {
+            MessageBox.Show("You clicked me at" + e.GetPosition(this).ToString());  //(this는 메인 윈도우) wpf에서 마우스 위치를 계산할 때
+        }
+    }
+}
---
+++ A013_Array/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace A013_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //배열 내용 정리
+            //int a[10];
+            //int b[] = { 1, 2, 3, 4, 5 }; //배열공간 컴파일 시 잡음
+            
+            int[] b = { 1, 2, 3, 4, 5 };
+            //TextBox[] c= {txt1, txt2, ..}  학점계산기 코딩 시 참고
+
+            for(int i = 0; i < b.Length; i++)  //배열도 class 왜냐하면 length라는 속성을 갖고 있으니까 때문에 class로 컨트롤가능
+                Console.WriteLine(b[i]);
+
+            Array.Sort(b);      //b.sort가 아니라 저렇게 멤버를 사용해야함.
+            for (int i = 0; i < b.Length; i++)  
+                Console.WriteLine(b[i]);
+
+            Console.WriteLine("Using for reach");
+            foreach (var i in b)   //b 배열에 있는 var(타입이 지정되지 않은 변수) 각각(각각의 요소)에 대해서 
+                Console.WriteLine(i);       //foreach (int i in b) 여기선 정수 이기 때문에 이렇게 작성해도 ok
+
+
+
+        }
+    }
+}
